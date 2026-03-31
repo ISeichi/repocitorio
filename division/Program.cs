df0ec8f82da07ca16dbdb72f1e68aa6f202c39e2
lizbeth <sarmientoperezlizbeth24@gmail.com>
2026-03-31 11:32:12 -0500

    Division

---
+++ division/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+class Division
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresar primer número");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingresar segundo número");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (a > 0)
+        {
+            int division = a / b ;
+            Console.WriteLine("La división es :" + division);
+        }
+        else
+        {
+            Console.WriteLine("La división no se puede realizar por ser número menor que 0");
+        }
+
+    }
+}
