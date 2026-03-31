69ffcac10ccd55c5d0126c862009a7aa78210a31
HalimaCornejo <halimacornejo@gmail.com>
2026-03-31 13:43:09 -0500

    adicion aplicada

--- adicion/Program.cs
+++ adicion/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Suma
+{
+    static void Main()
+    {
+        Console.WriteLine("Calculadora Suma");
+        Console.Write("Escribe el primer número: ");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Escribe el segundo número: ");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+        double resultado = n1 + n2;
+
+        Console.WriteLine($"La suma de {n1} y {n2} es: {resultado}");
+        if (resultado > 0)
+        {
+            Console.WriteLine("El resultado es positivo.");
+        }
+        else
+        {
+            Console.WriteLine("El resultado es negativo.");
+        }
+       
+    }
+}
