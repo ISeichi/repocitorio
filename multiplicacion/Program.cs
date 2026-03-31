05c7be415da372a2b41d37195eca51bc31312fbc
gtoledoa <N00518403@upn.pe>
2026-03-31 12:23:52 -0500

    Función multipluicación implementada

---
+++ multiplicacion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Multiplicacion
+{
+    static void Main()
+    {
+        double n1;
+        double n2;
+        double multi;
+        Console.WriteLine("Multiplicación");
+        Console.WriteLine("Escriba el primer númnero:");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Escriba el segundo número:");
+        n2 = Convert.ToDouble(Console.ReadLine()); 
+        multi = n1 * n2;
+        Console.WriteLine($"El resultado es: {multi}");
+    }
+}
